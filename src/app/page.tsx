69f6514e75f5cbf2ff8d94060695577033798359
cihanbuyukkara <buyukkara.cihan@gmail.com>
2024-06-18 00:34:30 +0300

    fixjj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from 'lucide-react';
+import { Check, Star } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -39,6 +39,20 @@ export default function Home() {
                  <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
                   <div className="flex -space-x-4" >
                     <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-1.png" alt='user image'/>
+                    <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-2.png" alt='user image'/>
+                    <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-3.png" alt='user image'/>
+                    <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-4.jpg" alt='user image'/>
+                    <img className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100" src="/users/user-5.jpg" alt='user image'/>
+                  </div>
+                  <div className="flex flex-col justify-between items-center sm:items-start">
+                    <div className="flex gap-0.5 ">
+                      <Star className= 'h4 w-4 text-green-600 fill-green-600'/>
+                      <Star className= 'h4 w-4 text-green-600 fill-green-600'/>
+                      <Star className= 'h4 w-4 text-green-600 fill-green-600'/>
+                      <Star className= 'h4 w-4 text-green-600 fill-green-600'/>
+                      <Star className= 'h4 w-4 text-green-600 fill-green-600'/>
+                    </div>
+                    <p><span className="font-semibold" >1.250</span>happy customer</p>
                   </div>
                 </div>  
             </div>
